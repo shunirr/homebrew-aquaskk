7459b6f432d92837cec482e97cdbe77615097e8a
shunirr <m@s5r.jp>
2014-03-13 14:53:33 +0900

    Initial commit

---
+++ Casks/aqua-skk.rb
@@ -0,0 +1,7 @@
+class AquaSkk < Cask
+  url 'http://jaist.dl.sourceforge.jp/aquaskk/58814/AquaSKK-4.2.dmg'
+  homepage 'http://aquaskk.sourceforge.jp/'
+  sha256 '2370ba379a1b5f4e5cc3bf3821ba396b713b7ff7ebfa0c2324c7a4c933324f1d'
+  version '4.2'
+  install 'AquaSKK.pkg'
+end
