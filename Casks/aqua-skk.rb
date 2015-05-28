b953a4589adbd73e437eccbe6dc6fb2324840d60
shunirr <m@s5r.jp>
2015-05-28 16:10:05 +0900

    update cask style

--- Casks/aqua-skk.rb
+++ Casks/aqua-skk.rb
@@ -1,4 +1,4 @@
-class AquaSkk < Cask
+cask :v1 => 'aquaskk' do
   url 'https://github.com/codefirst/aquaskk/releases/download/4.2.5/AquaSKK-4.2.5.dmg'
   homepage 'https://github.com/codefirst/aquaskk'
   sha256 '281e65c0687b0c558c26a4cb7929ac1dfe61dabfa75ac5b6be08bf4f53b49b86'
